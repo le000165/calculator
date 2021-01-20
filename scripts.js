589f00d0190a154cbe13aa5fce1b28951ff2b4e5
Phong Le <thephongvtvt123@gmail.com>
2021-01-19 20:40:04 -0500

    *** Complete Calculator Rel.1 ***

--- scripts.js
+++ scripts.js
@@ -9,16 +9,16 @@
 const buttons = Array.from(document.querySelectorAll('.btn'));
 const numberButtons = buttons.filter(numButton => !(isNaN(numButton.textContent)));
 const mathButtons = Array.from(document.querySelectorAll('.operator')); // + - x /
+const decimalButton = document.querySelector('.decimal');
 const equalButton = document.querySelector('.equal-sign');
 const clearButton = document.querySelector('.clear');
 const backSpaceButton = document.querySelector('.backspace');
 const displayScreen = document.querySelector('.display-text');
-
-const MATH_KEYS = ['+', '-', '*', '/'];
-const SPECIAL_KEYS = ['AC', 'C'];
-
-let savedUserInputs = []; // empty array to save whatever user is clicked
-let displayValue = '0';
+const message = document.querySelector('.message');
+const chechBox = document.querySelector('#checkBox');
+const body = document.getElementsByTagName("body")[0];
+const html = document.getElementsByTagName("html")[0];
+const calculatoContainer = document.querySelector('.calculator-container');
 
 let firstNums = [];
 let secondNums = [];
@@ -26,6 +26,8 @@ let firstOperand = null;
 let secondOperand = null;
 let operator = null;
 let isOperated = false;
+let currentOperand = null;
+let isEqualClicked = false;
 
 // =================== Operator math functions ===============
 function add(x, y) {
@@ -42,21 +44,27 @@ function multiply(...nums) {
 
 function divide(dividend, divisor) {
     if (0 == divisor) {
-        throw new Error("Oops! Cannot divide by zero.");
+        return 0;
     }
     let quotient = dividend / divisor;
     return quotient;
 }
 
-// lacking of percent operator
-// function percent(num)
+// percentage
+function percentage(x, y) {
+    if (x == null) {
+        return y / 100;
+    } else {
+        return x / 100;
+    }
+}
 /**
  * Operate function to call for each number
  */
 function operate(x, y, math) {
     //converting from string to integer
-    x = parseInt(x);
-    y = parseInt(y);
+    x = parseFloat(x);
+    y = parseFloat(y);
     let result = 0;
     //evalutate to call the right operation 
     switch (math) {
@@ -70,14 +78,20 @@ function operate(x, y, math) {
             result = multiply(x, y);
             break;
         case '÷':
-            result = divide(x, y);
+            if (y != 0) {
+                result = divide(x, y);
+            } else {
+                result = 0;
+                setMessage('Can not divided by 0');
+            }
             break;
         case '%':
-            result = divide(x, y);
+            result = percentage(x, y);
             break;
         default:
             break;
     }
+    result = Math.round((result + Number.EPSILON) * 100) / 100;
     return result;
 }
 
@@ -85,7 +99,9 @@ function operate(x, y, math) {
 // update display screen with a string
 function updateDisplay(text) {
     if (firstNums.length == 0 && secondNums.length == 0) {
-        text = 0;
+        if (text != '0.') {
+            text = 0;
+        }
     }
     if (text !== null) {
         displayScreen.textContent = text.toString();
@@ -93,25 +109,79 @@ function updateDisplay(text) {
     if (displayScreen.textContent.length > 13) {
         displayScreen.textContent = text.substring(0, 13);
     }
-
 }
 
+// clear function
 function clear() {
-    displayScreen.textContent = '0';
-    clearButton.style.backgroundColor = '#525252';
-    clearButton.style.color = '#fafafa';
+    setMessage('');
     firstNums = [];
     secondNums = [];
+    firstOperand = null;
+    secondOperand = null;
     operator = null;
+    isOperated = false;
+    currentOperand = null;
+    isEqualClicked = false;
+    updateDisplay('0');
+}
+
+// when equal is pressed
+function equalSignPressed() {
+    setMessage('');
+    // when ther are all requirements
+    if (getFirstOperand() !== null &&
+        getSecondOperand() !== null &&
+        getOperator() !== null) {
+        let result = operate(getFirstOperand(), getSecondOperand(), getOperator());
+        firstNums = [];
+        secondNums = [];
+        operator = null;
+        setFirstOperand(result);
+        isOperated = true;
+        isEqualClicked = true;
+    } else {
+        return;
+    }
+}
+
+// deleting 
+function backSpace() {
+    if (isEqualClicked) {
+        clear();
+    }
+    if (currentOperand === 'first') {
+        firstNums.pop();
+        updateDisplay(getFirstOperand());
+    } else {
+        secondNums.pop();
+        if (secondNums.length == 0) {
+            clear();
+        } else {
+            updateDisplay(getSecondOperand());
+        }
+    }
 }
 // checking if we have the first number
 // ======== SETTERS =========
 function setFirstOperand(x) {
+    currentOperand = 'first';
     firstNums.push(x);
+    updateDisplay(getFirstOperand());
+}
+
+function setMessage(myMessage) {
+    if (myMessage != '') {
+        message.textContent = myMessage;
+        message.style.visibility = 'visible';
+    } else {
+        message.style.visibility = 'hidden';
+    }
 }
 
 function setSecondOperand(y) {
+    currentOperand = 'second';
     secondNums.push(y);
+    updateDisplay(getSecondOperand());
 }
 
 function setOperator(op) {
@@ -137,36 +207,33 @@ function getOperator() {
     return operator;
 }
 
-// =================== Adding Event Listener ===============
+function getCurrentOperand() {
+    return currentOperand;
+}
 
+// =================== Adding Event Listener ===============
 // --------------- NUMBERS ------------
 numberButtons.forEach(b => b.addEventListener('click',
     (e) => {
         //when there is no operator, or first operator, it is the first operand
+        setMessage('');
         if (getOperator() == null) {
             if (isOperated) {
                 firstNums = [];
                 isOperated = false;
             }
             setFirstOperand(e.target.textContent);
-            updateDisplay(getFirstOperand());
-            console.log('opetator is null')
         } else {
             setSecondOperand(e.target.textContent);
-            updateDisplay(getSecondOperand());
-            console.log('opetator is not null')
         }
     }));
 
-// when a math operator is clicked
-// only + is on operation
 // --------------- MATHS ------------
 mathButtons.forEach(mathButton => mathButton.addEventListener('click',
     (e) => {
+        setMessage('');
         // when there is an exist operator
         if (getOperator() !== null) {
-            // console.log(getFirstOperand());
-            // console.log(getSecondOperand());
             // when the second number is set, 
             // and when the operator is clicked one more time
             if (getSecondOperand() !== null) {
@@ -174,57 +241,73 @@ mathButtons.forEach(mathButton => mathButton.addEventListener('click',
                 firstNums = [];
                 secondNums = [];
                 setFirstOperand(result);
-                updateDisplay(result);
                 isOperated = true;
             }
             setOperator(e.target.textContent);
+            if (getOperator() == '%' && getCurrentOperand() != null) {
+                let result = operate(getFirstOperand(), getSecondOperand(), '%');
+                firstNums = [];
+                secondNums = [];
+                setFirstOperand(result);
+            }
         } else {
             //when operator is never saved, then initialize it
             setOperator(e.target.textContent);
-            updateDisplay(getFirstOperand());
-            // console.log('----> set operator for the first time <----');
+            if (getOperator() == '%' && getCurrentOperand() != null) {
+                let result = operate(getFirstOperand(), getSecondOperand(), getOperator());
+                firstNums = [];
+                secondNums = [];
+                setFirstOperand(result);
+                isOperated = true;
+            }
         }
     }));
 
-// --------------- EQUAL SIGN ------------
-equalButton.addEventListener('click', equalSignPressed);
-
-function equalSignPressed() {
-    // when ther are all requirements
-    if (getFirstOperand() !== null &&
-        getSecondOperand() !== null &&
-        getOperator() !== null) {
-        let result = operate(getFirstOperand(), getSecondOperand(), getOperator());
-        firstNums = [];
-        secondNums = [];
-        operator = null;
-        setFirstOperand(result);
-        updateDisplay(result);
-        isOperated = true;
+// --------------- DECIMAL ------------
+decimalButton.addEventListener('click', function () {
+    // if pressing this at the begining
+    setMessage('');
+    if (getCurrentOperand() == null) {
+        setFirstOperand('0.');
+        // when there is number in first or second operand
     } else {
-        return;
+        if (getCurrentOperand() === 'first') {
+            if (isOperated) {
+                if (isEqualClicked) {
+                    firstNums = [];
+                    setFirstOperand('0.');
+                    isEqualClicked = false;
+                    isOperated = false;
+                } else {
+                    setSecondOperand('0.');
+                }
+            } else {
+                if (!getFirstOperand().includes('.')) {
+                    setFirstOperand('.')
+                }
+            }
+        } else {
+            if (!getSecondOperand().includes('.')) {
+                setSecondOperand('.')
+            }
+        }
     }
-}
-
-// --------------- CLEAR ALL ------------
-clearButton.addEventListener('click', clear);
-
-// --------------- BACK SPACE ------------
-backSpaceButton.addEventListener('click', function () {
-    if (secondNums.length > 0) {
-        secondNums.pop();
-        updateDisplay(getSecondOperand());
-    } else {
-        firstNums.pop();
-        updateDisplay(getFirstOperand());
-    }
-    console.log(firstNums);
-    console.log(firstNums);
 });
 
-// window.addEventListener('keydown', displayText);
+
+
+// --------------- SWITCHER ------------
+chechBox.addEventListener('click', function (e) {
+    body.classList.toggle('dark-mode');
+    html.classList.toggle('dark-mode');
+    calculatoContainer.classList.toggle('shadow-calculator');
+});
+
+
+
+equalButton.addEventListener('click', equalSignPressed);
+clearButton.addEventListener('click', clear);
+backSpaceButton.addEventListener('click', backSpace);
 
 // =================== SANDBOX ===============
-// console.log(operate(100, 5, multiply));
-
-updateDisplay(displayValue);
+updateDisplay('0');
